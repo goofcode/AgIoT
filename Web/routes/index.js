72f6bd95d3a08b979fee3faca60c165d9da0004a
greenmon <greenmon@kaist.ac.kr>
2018-11-17 00:08:16 -0500

    Add scripts to change cell on new packet arrival
    - API and jquery script to change each cell on DOM.
    - Add cache to store cells on server for entire session.

--- Web/routes/index.js
+++ Web/routes/index.js
@@ -1,15 +1,100 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const cache = require('memory-cache');
 
-var n = 10;
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', {num_row: n, num_col: n});
+const n = 10;
+
+const parseCellNumber = (data) => {
+    // TODO: properly parse cell number from packet
+    return data[0];
+};
+
+const parseImage = (data) => {
+    // TODO: properly parse image data
+    return data.slice(1);
+};
+
+const getImageCell = (data) => {
+    return {
+        number: parseCellNumber(data),
+        image: parseImage(data)
+    }
+};
+
+const updateCell = (res, changedCell) => {
+    let cells = cache.get('cells');
+    let updateQueue = cache.get('updateQueue');
+
+    if (!cells) {
+        cells = [];
+    }
+
+    cells[changedCell.number] = changedCell.image;
+
+    cache.put('cells', cells);
+
+    if (!updateQueue) {
+        updateQueue = [];
+    }
+
+    updateQueue.push(changedCell);
+
+    cache.put('updateQueue', updateQueue);
+};
+
+router.get('/', function (req, res) {
+    const cells = cache.get('cells');
+
+    // Draw initial image with whole cells saved in server
+    res.render('index', {
+        imageCells: cells ? cells : [],
+        num_row: n,
+        num_col: n
+    });
 });
 
-router.post('/api/image', function (req, res, next) {
-  res.send(req.body);
+router.get('/api/cells', function (req, res) {
+    const cells = cache.get('cells');
+
+    if (!cells) {
+        res.send([]);
+    } else {
+        res.send(cells);
+    }
 });
+
+router.get('/api/updatedCells', function (req, res) {
+    const updateQueue = cache.get('updateQueue');
+
+    if (!updateQueue) {
+        res.send([]);
+    } else {
+        res.send(updateQueue);
+    }
+});
+
+router.post('/api/updatedCells/pop', function (req, res) {
+    const updateQueue = cache.get('updateQueue');
+
+    updateQueue.shift();
+    cache.put('updateQueue', updateQueue);
+
+    res.send(cache.get('updateQueue'));
+});
+
+router.post('/api/image', async function (req, res) {
+    let changedCell = await getImageCell(req.body['cell']);
+
+    await updateCell(res, changedCell);
+
+    res.send({
+        changedCellNumber: changedCell.number,
+        imageData: changedCell.image,
+        cells: cache.get('cells'),
+        updateQueue: cache.get('updateQueue')
+    });
+});
+
 
 module.exports = router;
