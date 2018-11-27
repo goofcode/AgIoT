f8f03ede647715492090d743712595721970d2d9
Mookeun Ji <goofcode@gmail.com>
2018-11-27 00:52:01 -0500

    Add ack

--- LoRa_Server/LoRa_Server.ino
+++ LoRa_Server/LoRa_Server.ino
@@ -7,29 +7,47 @@
 #define   FREQUENCY     868.0
 
 
-RH_RF95     rf95;
-String      rest_url = "http://52.78.16.193:8080/api/image";
+RH_RF95           rf95;
+const char        rest_url[]    = "http://52.78.16.193:8080/api/image";
+const char        rest_header[] = "'Content-Type: application/x-www-form-urlencoded'";
 
 uint8_t buf[RH_RF95_MAX_MESSAGE_LEN+1];
 uint8_t len = RH_RF95_MAX_MESSAGE_LEN;
 
+uint8_t ack_buf[4] = " ACK";
+
 void api_send_cell(int number, uint8_t* cell)
 {
-  Process p;    
+  String data = String(F("number=")) + String(number) + String(F("&cell="));
+  
+  for(int i=0; i< 100; i++)
+    data += String(cell[i], HEX);
+
+  Process p;
   p.begin("curl");
-  p.addParameter("-X Post");
-  p.addParameter("-H content-type: multipart/form-data");
-  p.addParameter("-k " + upload_url);
-  p.addParameter("-F number="  );
-  p.addParameter(buf);
+  p.addParameter("-X");
+  p.addParameter("POST");
+  p.addParameter(rest_url);
+  p.addParameter("-H");
+  p.addParameter(rest_header);
+  p.addParameter("-d");
+  p.addParameter(data);
   p.run();
+  
+  // A process output can be read with the stream methods
+  while (p.available() > 0) {
+    char c = p.read();
+    Console.print(c);
+  } 
+  // Ensure the last bit of data is sent.
+  Console.flush();
 }
 
 void setup() 
 {
   Bridge.begin(BAUDRATE);
   Console.begin();
-  while (!Console) ;                // Wait for serial port to be available
+  while (!Console);                // Wait for serial port to be available
   
   if (!rf95.init()){
     Console.println("init failed");
@@ -37,37 +55,42 @@ void setup()
   }
   Console.println("Start LoRa Client");
 
-  rf95.setFrequency(frequency);     // Setup ISM frequency
+  rf95.setFrequency(FREQUENCY);     // Setup ISM frequency
   rf95.setTxPower(13);              // Setup Power,dBm
   rf95.setSpreadingFactor(7);       // Setup Spreading Factor (6 ~ 12)  
   rf95.setSignalBandwidth(125000);  // Setup BandWidth, option: 7800,10400,15600,20800,31200,41700,62500,125000,250000,500000
                                     // Lower BandWidth for longer distance.
   rf95.setCodingRate4(5);           // Setup Coding Rate:5(4/5),6(4/6),7(4/7),8(4/8) 
   
-  Console.print("Listening on frequency: ");
-  Console.println(frequency);
+  Console.print(F("Listening on frequency: "));
+  Console.println(FREQUENCY);
 }
 
 void loop()
-{
+{  
   if (rf95.available())
   {    
     if (rf95.recv(buf, &len))
-    {      
-      buf[len] = '\0';
-      
-      Console.print("packet received (rssi: ");
-      Console.println(rf95.lastRssi(), DEC);
-      Console.print(")");
-      
-      // Send a ACK
-      rf95.send("ACK", sizeof("ACK"));
-      rf95.waitPacketSent();
+    {            
+      Console.print(F("packet received "));
+      Console.println((int)len);
 
+      for(int i=0 ;i<len; i++){
+        Console.print((int)buf[i]);
+        Console.print(F(" "));
+      }
+      Console.println();
+
+      ack_buf[0] = buf[0];
+    
+      rf95.send(ack_buf, sizeof(ack_buf));
+      rf95.waitPacketSent();
+      
       // rest call
-//      api_send_cell(buf);
-       
+      api_send_cell(buf[0], &buf[1]);
+      
       Console.println();
     }
   }
+  
 }
