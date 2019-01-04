88692511edf5d303bc7e9640f9bb4bdd9a9abc7d
Mookeun Ji <goofcode@gmail.com>
2019-01-04 14:43:50 +0900

    Close project

--- LoRa_Client/main.py
+++ LoRa_Client/main.py
@@ -1,5 +1,6 @@
 import logging
 import time
+import datetime
 
 import numpy as np
 import picamera
@@ -10,8 +11,7 @@ camera_ready_sleep = 4
 sleep_period = 0.5
 intercell_sleep = 0.1
 
-thresholds = [100, 100, 0.5]  # manhattan, euclidean, cosine
-
+file_idx = 0
 
 class AtoI:
 
@@ -20,30 +20,31 @@ class AtoI:
         # ready logging
         self.logger = logging.getLogger('atoi')
         self.logger.setLevel(logging.DEBUG)
-        log_format = logging.Formatter("[%(asctime)s] %(message)s")
+        log_format = logging.Formatter("%(asctime)s\t%(message)s")
 
-        file_handler = logging.FileHandler('lora.log')
+        file_handler = logging.FileHandler(datetime.datetime.now().strftime('%Y-%m-%d-%H-%M') +'.log')
         file_handler.setFormatter(log_format)
         self.logger.addHandler(file_handler)
 
         console_handler = logging.StreamHandler()
         console_handler.setFormatter(log_format)
+        # console_handler.setLevel(logging.INFO)
         self.logger.addHandler(console_handler)
 
         self.logger.debug('=' * 50)
-        self.logger.debug('{}*{}, n:{}, diff:{}, re_tx:{}, ack_wait:{}'
-                          .format(width, height, n, diff_alg, re_tx, wait*100))
+        self.logger.info('{}*{}, n:{}, diff:{}, re_tx:{}, ack_wait:{}'
+                          .format(width, height, n, diff_alg, re_tx, wait * 100))
 
         # ready camera
         self._camera = picamera.PiCamera(resolution=(width, height))
         time.sleep(camera_ready_sleep)
         self._camera.color_effects = (128, 128)
         self._camera.rotation = 180
-        self.logger.debug("pi camera setup finished")
+        self.logger.info("pi camera setup finished")
 
         # ready lora
         self._lora_serial = serial.Serial('/dev/ttyUSB0', baudrate=115200)
-        self.logger.debug(self._lora_serial.read_until()[:-1])
+        self.logger.info(self._lora_serial.read_until()[:-1])
 
         self.n = n
         self.total_cell = n * n
@@ -60,35 +61,72 @@ class AtoI:
         self.ack_wait = wait
 
         self.diff_alg = diff_alg
-        self.threshold = thresholds[diff_alg]
+        # self.threshold = self._get_default_threshold()
+        self.threshold = 0.12
+        self.margin = 0
 
-        self.logger.debug("initialization finished")
+        self.pending = False
+        self.buffer = None
 
-    def capture_gray_2d(self):
+        self.logger.info('default threshold\t{}'.format(self.threshold))
 
+        self.file_idx = 0
+
+        self.logger.info("initialization finished")
+
+    def _capture_np(self):
         self._camera.capture(self._capture, 'rgb')
-        self.now = np.array([self._capture[i*3] for i in range(160*160)]).reshape(160, 160)
+        return np.array([self._capture[i * 3] for i in range(160 * 160)]).reshape(160, 160)
 
+    def capture(self):
+
+        self.now = self._capture_np()
         if self.prev is None:
             self.prev = np.copy(self.now)
 
-        self.logger.debug('capture')
+        self.logger.info('capture')
 
         self._save()
 
     def _send(self, payload, ack=True):
         # type: (bytearray, bool) -> int
-        self._lora_serial.write(chr(len(payload)))
-        self._lora_serial.write(payload)
 
-        if not ack:
-            self._lora_serial.write('s')
+        if not self.pending:
+            self.buffer = bytearray(payload)
+            self.pending = True
+            return -1
+
         else:
-            self._lora_serial.write('a')
-            self._lora_serial.write(chr(self.re_tx))
-            self._lora_serial.write(chr(self.ack_wait))
+            self.buffer.extend(payload)
 
-        return int(self._lora_serial.readline())
+            self._lora_serial.write(chr(len(self.buffer)))
+            self._lora_serial.write(self.buffer)
+
+            # print((self._lora_serial.readline()))
+            # print((self._lora_serial.readline()))
+
+            if not ack:
+                self._lora_serial.write('s')
+            else:
+                self._lora_serial.write('a')
+                self._lora_serial.write(chr(self.re_tx))
+                self._lora_serial.write(chr(self.ack_wait))
+
+            self.pending = False
+
+            return int(self._lora_serial.readline())
+
+        # self._lora_serial.write(chr(12))
+        # self._lora_serial.write(b"123456789012")
+        #
+        # if not ack:
+        #     self._lora_serial.write('s')
+        # else:
+        #     self._lora_serial.write('a')
+        #     self._lora_serial.write(chr(self.re_tx))
+        #     self._lora_serial.write(chr(self.ack_wait))
+        #
+        # return int(self._lora_serial.readline())
 
     def _get_pos(self, idx):
         r_start = int(idx / self.n) * self.cell_height
@@ -119,7 +157,7 @@ class AtoI:
 
         start_time = time.time()
         sent = self._send(bytearray(payload))
-        self.logger.debug('cell\t{}\t{}\t{},'.format(idx, sent, time.time() - start_time))
+        self.logger.debug('cell\t{}\t{}\t{:.2f}'.format(idx, sent, time.time() - start_time))
         return sent
 
     def send_all_cells(self):
@@ -129,27 +167,54 @@ class AtoI:
             time.sleep(intercell_sleep)
 
     def _save(self):
-        with open('output', 'wb') as f:
+        with open('out/output' + str(self.file_idx), 'wb') as f:
             f.write(self.now)
 
-    def _get_diff(self, pcell, ncell):
+        self.file_idx += 1
 
-        pcell = pcell.flatten()
-        ncell = ncell.flatten()
+    def _get_SSIM(self, img1, img2):
+        # type: (np.ndarray, np.ndarray) -> float
+
+        flat1 = img1.flatten()
+        flat2 = img2.flatten()
+
+        m_x = flat1.mean()
+        m_y = flat2.mean()
+        dev_x = flat1.std()
+        dev_y = flat2.std()
+
+        dev_xy = np.dot(flat1 - m_x, flat2 - m_y).sum() / len(flat1)
+
+        c1 = (0.01 * 255) ** 2
+        c2 = (0.03 * 255) ** 2
+
+        return ((2 * m_x * m_y + c1) * (2 * dev_xy + c2)) / (
+                (m_x ** 2 + m_y ** 2 + c1) * (dev_x ** 2 + dev_y ** 2 + c2))
+
+    def _get_MSE(self, img1, img2):
+        # type: (np.ndarray, np.ndarray) -> float
+
+        flat1 = img1.flatten()
+        flat2 = img2.flatten()
+
+        return ((flat2 - flat1)**2).mean()
+
+    def _get_DSSIM(self, img1, img2):
+        return (1 - self._get_SSIM(img1,img2))/2
+
+    def _get_diff(self, pcell, ncell):
+        # type: (np.ndarray, np.ndarray) -> float
+
+        pcell = pcell.flatten().astype(np.float)
+        ncell = ncell.flatten().astype(np.float)
 
         # euclidean distance
         if self.diff_alg == 0:
-            return ((ncell - pcell) ** 2).sum()
+            return np.math.sqrt(((ncell - pcell) ** 2).sum())
 
-        # manhattan distance
+        # DSSIM
         elif self.diff_alg == 1:
-            return (np.abs(ncell - pcell)).sum()
-
-        # cosine distance
-        elif self.diff_alg == 2:
-            pcell = pcell / np.linalg.norm(pcell)
-            ncell = ncell / np.linalg.norm(ncell)
-            return np.dot(pcell, ncell)
+            return self._get_DSSIM(pcell, ncell)
 
     def _update_prev(self, idx, cell):
         r_start, r_end, c_start, c_end = self._get_pos(idx)
@@ -157,27 +222,60 @@ class AtoI:
 
     def send_diff_and_update(self):
 
+        diffs = []
+
         for idx in range(self.total_cell):
 
             pcell = self.get_prev_cell(idx)
             ncell = self.get_now_cell(idx)
 
             diff = self._get_diff(pcell, ncell)
-            self.logger.debug('diff\t{}\t{}\t{}'.format(idx, diff, self.threshold))
+            self.logger.debug('diff\t{}\t{:.3f}\t{:.3f}'.format(idx, diff, self.threshold + self.margin))
 
-            if diff > self.threshold:
+            diffs.append(diff)
+
+            if diff > self.threshold + self.margin:
                 self.send_cell(idx, ncell)
                 self._update_prev(idx, ncell)
+
+        # self.update_threshold(diffs)
+        self.logger.info('image SSIM\t{:.5f}'.format(self._get_SSIM(self.prev, self.now)))
+        self.logger.info('image MSE\t{:.5f}'.format(self._get_MSE(self.prev, self.now)))
+
+    def _get_Tn(self, diffs):
+        return sum(diffs) / len(diffs)
+
+    def _get_default_threshold(self):
+
+        first = self._capture_np()
+        time.sleep(0.1)
+        second = self._capture_np()
+
+        diffs = []
+
+        for idx in range(self.total_cell):
+            fcell = self._get_cell(first, idx)
+            scell = self._get_cell(second, idx)
+
+            diffs.append(self._get_diff(fcell, scell))
+
+        return self._get_Tn(diffs)
+
+    def update_threshold(self, diffs):
+        alpha = 0.05
+        self.threshold = (1 - alpha) * self.threshold + alpha * self._get_Tn(diffs)
+        self.logger.info('threshold\t{}'.format(self.threshold))
 
 
 if __name__ == "__main__":
 
-    atoi = AtoI(160, 160, 16, 0, 2, 30)
+    atoi = AtoI(160, 160, n=16, diff_alg=1, re_tx=2, wait=30)
 
-    atoi.capture_gray_2d()
+    atoi.capture()
     atoi.send_all_cells()
 
     while True:
-        atoi.capture_gray_2d()
+        atoi.capture()
         atoi.send_diff_and_update()
         time.sleep(sleep_period)
+
--- LoRa_Client/test.py
+++ LoRa_Client/test.py
@@ -1,15 +1,156 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from PIL import Image
 
-with open('output', 'rb') as f:
+import os
+
+n = 16
+cell_width = 10
+cell_height = 10
+total_cell = 256
+
+
+def _get_pos(idx):
+    r_start = int(idx / n) * cell_height
+    r_end = r_start + cell_height
+    c_start = int(idx % n) * cell_width
+    c_end = c_start + cell_width
+
+    return r_start, r_end, c_start, c_end
+
+
+def _get_cell(img, idx):
+    r_start, r_end, c_start, c_end = _get_pos(idx)
+    return img[r_start:r_end, c_start:c_end]
+
+
+def get_all_cells(img):
+    return [(i, _get_cell(img, i)) for i in range(total_cell)]
+
+
+def _get_SSIM(img1, img2):
+    # type: (np.ndarray, np.ndarray) -> float
+
+    flat1 = img1.flatten()
+    flat2 = img2.flatten()
+
+    m_x = flat1.mean()
+    m_y = flat2.mean()
+    dev_x = flat1.std()
+    dev_y = flat2.std()
+
+    dev_xy = np.dot(flat1 - m_x, flat2 - m_y).sum() / len(flat1)
+
+    c1 = (0.01 * 255) ** 2
+    c2 = (0.03 * 255) ** 2
+
+    return ((2 * m_x * m_y + c1) * (2 * dev_xy + c2)) / (
+            (m_x ** 2 + m_y ** 2 + c1) * (dev_x ** 2 + dev_y ** 2 + c2))
+
+
+def _get_man(img1, img2):
+    img1 = img1.flatten()
+    img2 = img2.flatten()
+    return (np.abs(img1 - img2)).sum()
+
+
+def _get_cos(img1, img2):
+    img1 = img1.flatten()
+    img2 = img2.flatten()
+
+    img1 = img1 / np.linalg.norm(img1)
+    img2 = img2 / np.linalg.norm(img2)
+
+    return (1 - np.dot(img1, img2)) / 2
+
+
+def _get_eucl(img1, img2):
+    # type: (np.ndarray, np.ndarray) -> float
+
+    flat1 = img1.flatten()
+    flat2 = img2.flatten()
+
+    return np.math.sqrt(((flat1 - flat2) ** 2).sum())
+
+
+def _get_DSSIM(img1, img2):
+    return (1 - _get_SSIM(img1, img2)) / 2
+
+
+def _update_prev(i, update, cell):
+    r_start, r_end, c_start, c_end = _get_pos(i)
+    update[r_start:r_end, c_start: c_end] = cell
+
+
+def _save(name, img):
+    pil = Image.fromarray(img)
+    pil.save('img/' + name + '.png')
+
+with open('out/prev', 'rb') as f:
     img = f.read()
+    prev = np.array(list(img)).reshape((160,160)).astype(np.uint8)
 
-img = np.array(list(img))
+with open('out/now', 'rb') as f:
+    img = f.read()
+    now = np.array(list(img)).reshape((160,160)).astype(np.uint8)
 
-print (img.shape)
-img = np.reshape(img, (100,100))
+man = np.copy(prev)
+cos = np.copy(prev)
+euc = np.copy(prev)
+dssim = np.copy(prev)
 
-print(img)
+man_thresh = 10000
+cos_thresh = 0.001
+eucl_thresh = 12
+dssim_thresh = 0.067
 
-plt.imshow(img, cmap=plt.get_cmap('gray'))
-plt.show()
+counts = [0, 0, 0, 0]
+
+for idx in range(total_cell):
+
+    pcell = _get_cell(prev, idx)
+    ncell = _get_cell(now, idx)
+
+    man_diff = _get_man(pcell, ncell)
+    if man_diff > man_thresh:
+        _update_prev(idx, man, ncell)
+        counts[0] += 1
+
+    cos_diff = _get_cos(pcell, ncell)
+    if cos_diff > cos_thresh:
+        _update_prev(idx, cos, ncell)
+        counts[1] += 1
+
+    euc_diff = _get_eucl(pcell, ncell)
+    if euc_diff > eucl_thresh:
+        _update_prev(idx, euc, ncell)
+        counts[2] += 1
+
+    dssim_diff = _get_DSSIM(pcell, ncell)
+    if dssim_diff > dssim_thresh:
+        _update_prev(idx, dssim, ncell)
+        counts[3] += 1
+
+    print('{}\t{}\t{}\t{}'.format(man_diff, cos_diff, euc_diff, dssim_diff))
+
+print(counts)
+
+_save('prev', prev)
+_save('now', now)
+_save('man', man)
+_save('cos', cos)
+_save('euc', euc)
+_save('dssim', dssim)
+
+# for file in os.listdir('out'):
+#     filename = 'out/' + file
+#
+    # with open(filename, 'rb') as f:
+    #     img = f.read()
+    #     img = np.array(list(img)).reshape((160,160))
+    #     plt.imshow(img, cmap=plt.get_cmap('gray'))
+    #     plt.title(file)
+    #     plt.savefig('img/' + file + '.png')
+    #     arr = np.array(list(img)).reshape((160,160)).astype(np.uint8)
+    #     pil = Image.fromarray(arr)
+    #     pil.save('img/' + file + '.png')
